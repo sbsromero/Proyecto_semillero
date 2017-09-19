9ad5839e2a3aedb076551d0d8a71258ad1a58d8b
sromero <sromero@sumset.com>
2017-09-18 22:25:11 -0500

    cambios

---
+++ server/semilleroUniversitario/src/Semillero/SemillasBundle/Controller/SemillaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Semillero\SemillasBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SemillaController extends Controller
+{
+}
