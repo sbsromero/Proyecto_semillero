23cf82b7c6e411e47072e44fe072117344c6d3e0
sromero <sromero@sumset.com>
2018-01-14 16:12:05 -0500

    Parte administrativa mentor

--- server/semilleroUniversitario/src/Semillero/UsuariosBundle/Controller/UsuariosController.php
+++ server/semilleroUniversitario/src/Semillero/UsuariosBundle/Controller/UsuariosController.php
@@ -4,6 +4,8 @@ namespace Semillero\UsuariosBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
 * @Route("/usuarios")
@@ -15,6 +17,61 @@ class UsuariosController extends Controller
   */
   public function dashboardUsuariosAction()
   {
-    return $this->render('UsuariosBundle:Dashboard:dashboardUsuarios.html.twig');
+    if($this->isGranted('IS_AUTHENTICATED_FULLY')){
+      return $this->render('UsuariosBundle:Dashboard:dashboardUsuarios.html.twig');
+    }
+    return $this->redirectToRoute('usuariosLogin');
+  }
+
+  //Metodo que se encarga de administrar los grupos asignados a un mentor
+  /**
+  * @Route("/index", name="indexGrupos_usuarios")
+  */
+  public function indexGrupos (Request $request)
+  {
+    if($this->isGranted('IS_AUTHENTICATED_FULLY')){
+      $em = $this->getDoctrine()->getManager();
+      $user = $this->container->get('security.context')->getToken()->getUser();
+      $gruposPorMentor = $em->getRepository('DataBundle:Mentor_Grupos')->gruposAsignadosPorMentor($user->getId());
+      return $this->render('UsuariosBundle:Grupos:indexGrupos.html.twig',array(
+        'user' => $user,
+        'gruposPorMentor' => $gruposPorMentor
+      ));
+    }
+    return $this->redirectToRoute('usuariosLogin');
+  }
+
+  /**
+  * @Route("/gestionGruposAsignados", name="gestionGruposAsignados")
+  */
+  public function gestionGruposAsignados (Request $request){
+    if($this->isGranted('IS_AUTHENTICATED_FULLY')){
+      $em = $this->getDoctrine()->getManager();
+      $idGrupo = $request->query->get('idGrupo');
+      $grupo = $em->getRepository('DataBundle:Grupo')->find($idGrupo);
+
+      return $this->render('UsuariosBundle:Grupos:administracionGrupos.html.twig',array(
+        'grupo' => $grupo
+      ));
+    }
+    return $this->redirectToRoute('usuariosLogin');
+  }
+
+  /**
+  * @Route("/datosPersonales",name="verDatosPersonales")
+  */
+  public function verDatosPersonales(Request $request){
+    if($this->isGranted('IS_AUTHENTICATED_FULLY')){
+      if ($request->isXmlHttpRequest()) {
+        $em = $this->getDoctrine()->getManager();
+        $mentor = $this->container->get('security.context')->getToken()->getUser();
+
+        return $this->render('MentoresBundle:Mentor:view.html.twig',array(
+          'mentor' => $mentor
+        ));
+      }
+      return $this->redirectToRoute('gestionGruposAsignados');
+    }
+    return $this->redirectToRoute('usuariosLogin');
   }
 }
