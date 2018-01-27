149aad303e085c22c2ccacbbf1052cab6307314c
sromero <sromero@sumset.com>
2018-01-27 18:10:14 -0500

    dump service controller

--- server/semilleroUniversitario/app/AppKernel.php
+++ server/semilleroUniversitario/app/AppKernel.php
@@ -34,9 +34,9 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle(),
         );
 
+        $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+        $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
