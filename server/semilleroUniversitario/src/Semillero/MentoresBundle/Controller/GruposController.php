49cb80e61cc37410ea831d4ac9f0f22b7986f484
sromero <sromero@sumset.com>
2018-01-27 21:35:34 -0500

    dump service controller

--- server/semilleroUniversitario/src/Semillero/MentoresBundle/Controller/GruposController.php
+++ server/semilleroUniversitario/src/Semillero/MentoresBundle/Controller/GruposController.php
@@ -244,58 +244,38 @@ class GruposController extends Controller
   }
 
   //Metodo que permite generar un pdf con las semillas de un grupo
-  /**
-  * @Route("/getPdfGrupoSemillas",name="getPdfGrupoSemillas")
-  */
-  public function getPdfGrupoSemillas(Request $request){
-    $idGrupo = $request->query->get('id');
-    $em = $this->getDoctrine()->getManager();
-    $grupo = $em->getRepository('DataBundle:Grupo')->find($idGrupo);
-    $m_g = $em->getRepository('DataBundle:Mentor_Grupos')->getMentorAsignadoPorGrupo($idGrupo);
-    $mentor = (empty($m_g)) ? null : $m_g->getMentor();
-
-    $grupo_semillas = $grupo->getSemillas();
-    $semillas = array();
-    foreach ($grupo_semillas as $grupo_semilla) {
-      if($grupo_semilla->getActivo()){
-        array_push($semillas, $grupo_semilla->getSemilla());
-      }
-    }
-    // $nombreArchivo = str_replace(' ','_',$grupo->getNombre());
-    $nombreArchivo = str_replace(' ','_',$this->quitar_tildes($grupo->getNombre()));
-    // dump("cambio, vamos a ver");
-    // dump($this->get('kernel')->getRootDir().'/../web'.$request->getBasePath(),$grupo,$mentor,$semillas);
-    // dump("datos de html");
-    // dump("datos html");
-    // dump("Nombre grupo",$grupo->getNombre(),"Jornada",$grupo->getJornada()->getNombre(),"Mentor",$mentor->getNombre());
-    // dump("Estado grupo",$grupo->getActivo(),"semilla",count($semillas));
-    // dump($this->get('kernel')->getRootDir().'/../web'.$request->getBasePath());
-    // exit();
-    // $algo = new PdfResponse(
-    //      $this->get('knp_snappy.pdf')->getOutputFromHtml($this->renderView('MentoresBundle:Grupo:plantillaPdfGrupoSemillas.html.twig', array(
-    //          'base_dir' => $this->get('kernel')->getRootDir().'/../web'.$request->getBasePath(),
-    //          'grupo' => $grupo,
-    //          'mentor' => $mentor,
-    //          'semillas' => $semillas,
-    //      ))),
-    //      'semillas_'.$nombreArchivo.'.pdf'
-    //  );
-    //  dump("Fallo",$algo);
-    //  exit();
-    return $this->render('MentoresBundle:Grupo:plantillaPdfGrupoSemillas.html.twig',array(
-      'grupo' => null,
-      'mentor' => null,
-      'semillas' => null,
-      'base_dir' => null
-    ));
-  }
-
-  function quitar_tildes($cadena) {
-    $no_permitidas= array ("á","é","í","ó","ú","Á","É","Í","Ó","Ú","ñ","À","Ã","Ì","Ò","Ù","Ã™","Ã ","Ã¨","Ã¬","Ã²","Ã¹","ç","Ç","Ã¢","ê","Ã®","Ã´","Ã»","Ã‚","ÃŠ","ÃŽ","Ã”","Ã›","ü","Ã¶","Ã–","Ã¯","Ã¤","«","Ò","Ã","Ã„","Ã‹");
-    $permitidas= array ("a","e","i","o","u","A","E","I","O","U","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E");
-    $texto = str_replace($no_permitidas, $permitidas ,$cadena);
-    return $texto;
-  }
+  // /**
+  // * @Route("/getPdfGrupoSemillas",name="getPdfGrupoSemillas")
+  // */
+  // public function getPdfGrupoSemillas(Request $request){
+  //   $idGrupo = $request->query->get('id');
+  //   $em = $this->getDoctrine()->getManager();
+  //   $grupo = $em->getRepository('DataBundle:Grupo')->find($idGrupo);
+  //   $m_g = $em->getRepository('DataBundle:Mentor_Grupos')->getMentorAsignadoPorGrupo($idGrupo);
+  //   $mentor = (empty($m_g)) ? null : $m_g->getMentor();
+  //
+  //   $grupo_semillas = $grupo->getSemillas();
+  //   $semillas = array();
+  //   foreach ($grupo_semillas as $grupo_semilla) {
+  //     if($grupo_semilla->getActivo()){
+  //       array_push($semillas, $grupo_semilla->getSemilla());
+  //     }
+  //   }
+  //   return new PdfResponse(
+  //        $this->get('knp_snappy.pdf')->getOutputFromHtml($this->renderView('MentoresBundle:Grupo:plantillaPdfGrupoSemillas.html.twig', array(
+  //            'base_dir' => $this->get('kernel')->getRootDir().'/../web'. $request->getBasePath(),
+  //            'grupo' => $grupo,
+  //            'mentor' => $mentor,
+  //            'semillas' => $semillas,
+  //        ))),
+  //        'semillas-'.trim($grupo->getNombre()).'.pdf'
+  //    );
+  //   // return $this->render('MentoresBundle:Grupo:plantillaPdfSemillas.html.twig',array(
+  //   //   'grupo' => $grupo,
+  //   //   'semillas' => $semillas,
+  //   //   'base_dir' => $this->get('kernel')->getRootDir().'/../web'. $request->getBasePath()
+  //   // ));
+  // }
 
   //Metodo que permite generar un pdf con todos los grupos registrados
   /**
